a8b9a3b3742fad09d818b4c06498b620e62e4bac
bilal asghar <asgharbilal8@gmail.com>
2025-04-04 16:59:27 +0100

    index.js code cleanup and created .env

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 const express = require("express");
-const app = express();
 const path = require('path');
 const mustache = require('mustache-express');
+const cookieParser = require('cookie-parser')
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, './public')));
 
-// Generic function to serve all static files from the "public" directory
-app.use(express.static(path.join(__dirname, "node_modules")));
-app.use(express.static(path.join(__dirname, "public")));
+const app = express();
+require('dotenv').config() //load data from .env file
+app.use(cookieParser())
 
-// Set up mustache for templating
-app.engine('mustache', mustache());
-app.set('view engine', 'mustache');
+Helpers_SetupMustache();
+
+Helpers_ParsingMiddleware();
+Helpers_ServeStaticFiles();
 
 const router = require('./routes/IndexRoutes');
 app.use('/', router);
 app.listen(3000, () => {
     console.log('Server listening on port: 3000');
 });
+
+
+//#region Helpers
+
+function Helpers_SetupMustache() {
+    // Set up mustache for templating
+    app.engine('mustache', mustache());
+    app.set('view engine', 'mustache');
+}
+
+function Helpers_ParsingMiddleware() {
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
+}
+
+function Helpers_ServeStaticFiles() {
+    app.use(express.static(path.join(__dirname, "node_modules")));
+    app.use(express.static(path.join(__dirname, "public")));
+}
+
+//#endregion
